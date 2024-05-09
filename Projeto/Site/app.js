fbb7d106ff67fff2b14e6dfc09ab084082597f09
DiegoCampos2507 <diego.campos@sptech.school>
2024-05-08 21:40:08 -0300

    Modificando tela de cadastro

--- Projeto/Site/app.js
+++ Projeto/Site/app.js
@@ -17,14 +17,6 @@ var app = express();
 
 var userRouter = require("./src/routes/user")
 
-/*
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
-*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,14 +26,6 @@ app.use(cors());
 
 app.use("/user", userRouter)
 
-/*
-app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
-*/
 
 app.listen(PORTA_APP, function () {
     console.log(`
