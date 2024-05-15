2a49b07ec16fe853251524e84c709f06f091da6b
DiegoCampos2507 <diego.campos@sptech.school>
2024-05-15 13:33:23 -0300

    Trazendo dados para a página perfil / continuando estilização página perfil

--- Well-Being/Site/src/database/script-tabelasWellBeing.sql
+++ Well-Being/Site/src/database/script-tabelasWellBeing.sql
@@ -11,4 +11,30 @@ CREATE TABLE usuario (
     senha VARCHAR(45)
 );
 
-SELECT * FROM usuario;
+CREATE TABLE grupo (
+	idGrupo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
+);
+
+CREATE TABLE GrupoUsuario (
+	idGrupoUsuario INT auto_increment PRIMARY KEY,
+    fkGrupo INT,
+    fkUsuario INT,
+    CONSTRAINT fkGrupo FOREIGN KEY (fkGrupo) REFERENCES grupo(idGrupo),
+    CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+INSERT INTO grupo VALUES 
+(default, "Ansiedade"),
+(default, "Depressão"),
+(default, "TDAH"),
+(default, "Transtorno Alimentar"),
+(default, "TOC"),
+(default, "Outras");
+
+SELECT * FROM usuario LEFT JOIN GrupoUsuario ON fkUsuario = idUsuario;
+
+INSERT INTO GrupoUsuario VALUES 
+(default, 1, 1);
+
+SELECT email, senha, usuario.nome, count(fkUsuario) FROM usuario JOIN GrupoUsuario ON fkUsuario = idUsuario WHERE email = 'diego@gmail.com' AND senha = '12345678' GROUP BY fkUsuario;
--- Well-Being/Site/src/models/userModel.js
+++ Well-Being/Site/src/models/userModel.js
@@ -1,24 +1,27 @@
-var database = require("../database/config")
-
+var database = require("../database/config");
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucaoSql = `
-        SELECT email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ",
+    email,
+    senha
+  );
+  var instrucaoSql = `
+  SELECT email, senha, usuario.nome, count(fkUsuario) AS grupos FROM usuario JOIN GrupoUsuario ON fkUsuario = idUsuario WHERE email = '${email}' AND senha = '${senha}' GROUP BY fkUsuario;
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function cadastrar(nome, sobrenome, nasc, email, senha) {
-    var instrucao = `
+  var instrucao = `
         INSERT INTO usuario (nome, sobrenome , dtNasc, email, senha) VALUES ('${nome}', '${sobrenome}', '${nasc}', '${email}', '${senha}');
     `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
 module.exports = {
-    cadastrar,
-    autenticar
+  cadastrar,
+  autenticar,
 };
